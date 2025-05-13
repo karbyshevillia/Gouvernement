6fa093dd685abdb6cde5410d081ceef2229fef3f
karbyshevillia <158076825+karbyshevillia@users.noreply.github.com>
2025-05-13 18:52:47 +0300

    Database class object initialised (SQLAlchemy) in __init__.py, database models for Users and User Notes added in models.py with the User-Note relationship being one-to-many

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     """
@@ -7,6 +11,8 @@ def create_app():
 
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "314159265358979323846264339"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
