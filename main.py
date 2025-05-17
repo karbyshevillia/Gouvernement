ba48dff038ca9c9d1021681e9061a24533fae5aa
karbyshevillia <158076825+karbyshevillia@users.noreply.github.com>
2025-05-17 21:21:09 +0300

    Added automatic cache clear-up after program exit

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from Gouvernement import create_app
+import atexit
+import subprocess
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
+
+    @atexit.register
+    def cleanup_pycache():
+        subprocess.run(['pyclean', os.getcwd()], check=False)
+
+
     app.run(debug=True)
