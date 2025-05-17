7593c67866a8a6008161eab6c758fb4b2f5a6655
karbyshevillia <158076825+karbyshevillia@users.noreply.github.com>
2025-05-18 00:33:04 +0300

    Added the Tasks page and a few html pages to improve navigation between Projects and Tasks. Also added a logo.

--- Gouvernement/tasks.py
+++ Gouvernement/tasks.py
@@ -18,7 +18,8 @@ from flask_login import (login_user,
                          login_required,
                          logout_user,
                          current_user)
-from .aux.tools import collaborators_input_is_valid
+from .aux.tools import (collaborators_input_is_valid,
+                        task_search)
 from .aux.user_rights import (role_required,
                               is_task_assigner,
                               is_task_assignee,
@@ -28,8 +29,28 @@ from .aux.user_rights import (role_required,
 from .aux.task_rights import (attr_required,
                               project_is_open)
 from datetime import datetime
+from sqlalchemy import or_, and_
 
 tasks = Blueprint("tasks", __name__)
+
+@tasks.route("/tasks", methods=["GET", "POST"])
+@login_required
+def tasks_view():
+    """
+    Returns the Tasks page of the Gouvernement
+    """
+    tasks_list = Task.query.filter(or_(Task.assigned_by == current_user.id,
+                                       Task.current_assignees.contains(current_user)))
+    if request.method == "POST":
+        search_string = request.form.get("filters")
+        tasks_list = task_search(search_string, tasks_list)
+
+    parent_projects_list = [Project.query.get(task.parent_project) for task in tasks_list]
+    zipped = list(zip(tasks_list, parent_projects_list))
+    tasks_assigners = dict(zip(zipped, [User.query.get(task.assigned_by) for task in tasks_list]))
+    return render_template("tasks/tasks.html",
+                           user=current_user,
+                           tasks_assigners=tasks_assigners)
 
 
 @tasks.route("/project/<int:project_id>/initiate", methods=["GET", "POST"])
@@ -112,6 +133,28 @@ def task_info(project_id, task_id):
                            parent_project_id=project_id)
 
 
+@tasks.route("/task/<int:task_id>")
+@login_required
+@role_required(is_task_assigner_or_assignee,
+               "You have to have assigned this task or be an assignee to be able to view its info.",
+               "/views/projects")
+def task_disjoint_info(task_id):
+    """
+    Shews information about a Task as accessed
+    through a Project Info page by clicking on a Task
+    """
+    task = Task.query.get(task_id)
+    project_id = task.parent_project
+    assigned_by = User.query.get(task.assigned_by).email
+    assignee_emails = [user.email for user in task.current_assignees]
+
+    return render_template("tasks/tasks_disjoint_info.html",
+                           user=current_user,
+                           task=task,
+                           assigned_by=assigned_by,
+                           assignee_emails=assignee_emails,
+                           parent_project_id=project_id)
+
 @tasks.route("/project/<int:project_id>/task/<int:task_id>/edit", methods=["GET", "POST"])
 @login_required
 @role_required(is_task_assigner,
@@ -187,3 +230,35 @@ def delete_task(project_id, task_id):
     db.session.commit()
     flash(f"Task {was} has been deleted from project {was_project}.", "success")
     return redirect(f"/views/projects/{project_id}")
+
+@tasks.route("/task/<int:task_id>/parent=<int:project_id>", methods=["GET", "POST"])
+@login_required
+@role_required(is_project_supervisor_or_collaborator,
+               "You have to supervise this project or be its current collaborator to be able to view its info.",
+               "/tasks/tasks")
+def project_from_task_info(task_id, project_id):
+    """
+    Shews information about a Project as accessed
+    through the Tasks tab by clicking on 'See Project Info'
+    in Task Info
+    """
+    project = Project.query.get(project_id)
+    supervisor = User.query.get(project.supervisor).email
+    collaborator_emails = [user.email for user in project.current_collaborators]
+
+    tasks_list = Task.query.filter(and_(or_(Task.assigned_by == current_user.id,
+                                       Task.current_assignees.contains(current_user)),
+                                       Task.parent_project == project_id))
+    if request.method == "POST":
+        search_string = request.form.get("filters")
+        tasks_list = task_search(search_string, tasks_list)
+
+    tasks_assigned_by = dict(zip(tasks_list, [User.query.get(task.assigned_by) for task in tasks_list]))
+
+    return render_template("projects/project_from_task_info.html",
+                           user=current_user,
+                           project=project,
+                           supervisor=supervisor,
+                           collaborator_emails=collaborator_emails,
+                           tasks_assigned_by=tasks_assigned_by,
+                           referrer_task_id=task_id)
